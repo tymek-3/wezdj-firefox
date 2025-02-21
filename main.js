aab0638039e86cd92711ee6e1161db5f7823c6ed
tymek-3 <tymekg3@gmail.com>
2025-02-21 11:38:45 +0100

    zbiera zdj, nie pobiera

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ browser.runtime.onMessage.addListener(msg => {
     console.log(`msg: ${msg}`)
     if (msg == "run") {
         console.log("message is run")
-        browser.tabs.query({ active: true, currentWindow: true }, tabs => {
-            const imgs = document.querySelectorAll("[class^='slider--img']>img")
+        const imgs = document.querySelectorAll("[class^='slider--img']>img")
 
-            let urls = []
-            imgs.forEach((el) => {
-                let url = el.getAttribute("src")
-                urls.push(`${url}\n`)
-            })
+        let urls = []
+        imgs.forEach((el) => {
+            let urlAttr = el.getAttribute("src")
 
-            const tab = tabs[0]
-            browser.tabs.sendMessage(tab.id, "test", urls).then(() => { console.log("sent") })
+            let ext;
+            if (urlAttr.includes(".jpg")) {
+                ext = ".jpg"
+            } else if (urlAttr.includes(".png")) {
+                ext = ".png"
+            } else {
+                console.log("none")
+                return
+            }
+
+            let idx = urlAttr.indexOf(ext)
+            let url = urlAttr.slice(0, idx + 4)
+            console.log(url)
+            urls.push(`${url}\n`)
         })
-        console.log("run end")
+
+        imgs.forEach(url => {
+            browser.downloads.download({ url: url });
+        })
+
     }
 })
-
-const test = "kajshdfklsjahd"
--- popup.js
+++ popup.js
@@ -1,18 +1,7 @@
 console.log("popup.js")
 
-function urlsToP() {
-    console.log("urlsToP")
-}
-
-console.log(window.test)
-
-// browser.runtime.onMessage.addListener(msg => {
-//     console.log("got message")
-//     console.log(msg)
-// })
-//
-// document.querySelector("button").addEventListener("click", () => {
-//     browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//         browser.tabs.sendMessage(tabs[0].id, "run")
-//     })
-// })
+document.querySelector("button").addEventListener("click", () => {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, "run")
+    })
+})
