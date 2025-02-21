72aaa4112bb3f0d8c86133a1d530bac2585ad41e
tymek-3 <tymekg3@gmail.com>
2025-02-21 15:11:53 +0100

    pobiera

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-console.log("main.js")
 browser.runtime.onMessage.addListener(msg => {
-    console.log(`msg: ${msg}`)
-    if (msg == "run") {
-        console.log("message is run")
-        const imgs = document.querySelectorAll("[class^='slider--img']>img")
+	if (msg == "run") {
+		const imgs = document.querySelectorAll("[class^='slider--img']>img")
 
-        let urls = []
-        imgs.forEach((el) => {
-            let urlAttr = el.getAttribute("src")
+		let urls = []
+		imgs.forEach((el) => {
+			let urlAttr = el.getAttribute("src")
 
-            let ext;
-            if (urlAttr.includes(".jpg")) {
-                ext = ".jpg"
-            } else if (urlAttr.includes(".png")) {
-                ext = ".png"
-            } else {
-                console.log("none")
-                return
-            }
+			let ext;
+			if (urlAttr.includes(".jpg")) {
+				ext = ".jpg"
+			} else if (urlAttr.includes(".png")) {
+				ext = ".png"
+			} else {
+				console.log("no extension")
+				return
+			}
 
-            let idx = urlAttr.indexOf(ext)
-            let url = urlAttr.slice(0, idx + 4)
-            console.log(url)
-            urls.push(`${url}\n`)
-        })
+			let idx = urlAttr.indexOf(ext)
+			let url = urlAttr.slice(0, idx + 4)
+			urls.push(`${url}\n`)
+		})
 
-        imgs.forEach(url => {
-            browser.downloads.download({ url: url });
-        })
-
-    }
+		browser.runtime.sendMessage({ action: "urls", data: urls })
+	}
 })
--- popup.js
+++ popup.js
@@ -1,7 +1,18 @@
-console.log("popup.js")
-
 document.querySelector("button").addEventListener("click", () => {
-    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        browser.tabs.sendMessage(tabs[0].id, "run")
-    })
+	browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		browser.tabs.sendMessage(tabs[0].id, "run")
+	})
+})
+
+browser.runtime.onMessage.addListener(msg => {
+	if (msg.action == "urls") {
+		console.log(msg.data)
+		const p = document.querySelector("p")
+
+		msg.data.forEach((u) => {
+			console.log(u)
+			p.innerHTML += `<a href="${u}">${u}</a><br>`
+			browser.downloads.download({ url: u });
+		})
+	}
 })
